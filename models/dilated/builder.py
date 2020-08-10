c6095dd59395629f390ede867ce498b23a1ac7f8
Yuntao Chen <chenyuntao08@gmail.com>
2020-08-10 13:29:42 +0000

    add resnext

--- models/dilated/builder.py
+++ models/dilated/builder.py
@@ -1,7 +1,6 @@
 import mxnet as mx
 import mxnext as X
 from mxnext import conv, relu, add
-from mxnext.backbone.resnet_v1b_helper import resnet_unit
 from symbol.builder import Backbone, Neck
 
 
@@ -20,6 +19,31 @@ class DilatedResNetV1bC4(Backbone):
         c2 = helper.resnet_c2(c1, num_c2, 1, p.c2_dil or 1, p.normalizer)
         c3 = helper.resnet_c3(c2, num_c3, 2, p.c3_dil or 1, p.normalizer)
         c4 = helper.resnet_c4(c3, num_c4, 2, p.c4_dil or 1, p.normalizer)
+
+        self.symbol = c4
+
+    def get_rpn_feature(self):
+        return self.symbol
+
+    def get_rcnn_feature(self):
+        return self.symbol
+
+
+class DilatedResNeXtC4(Backbone):
+    def __init__(self, pBackbone):
+        super().__init__(pBackbone)
+        p = self.p
+
+        import mxnext.backbone.resnext_helper as helper
+        num_c2, num_c3, num_c4, _ = helper.depth_config[p.depth]
+
+        data = X.var("data")
+        if p.fp16:
+            data = data.astype("float16")
+        c1 = helper.resnext_c1(data, p.normalizer)
+        c2 = helper.resnext_c2(c1, num_c2, 1, p.c2_dil or 1, 32, 4, p.normalizer)
+        c3 = helper.resnext_c3(c2, num_c3, 2, p.c3_dil or 1, 32, 4, p.normalizer)
+        c4 = helper.resnext_c4(c3, num_c4, 2, p.c4_dil or 1, 32, 4, p.normalizer)
 
         self.symbol = c4
 
